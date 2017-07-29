78a20eb848641a4522a928ee3fdbd49d071f3265
DESKTOP-PFBINLT\mitmoon <mitmoon@gmail.com>
2017-07-29 15:07:03 +0900

    AOP까지

--- src/main/java/com/springinaction/knights/Minstrel.java
+++ src/main/java/com/springinaction/knights/Minstrel.java
@@ -1,7 +1,26 @@
 package com.springinaction.knights;
 
+import java.io.PrintStream;
+
 /**
  * Created by mitmo on 2017-07-29.
  */
 public class Minstrel {
+    private PrintStream stream;
+
+    public Minstrel(){
+
+    }
+
+    public Minstrel(PrintStream stream) {
+        this.stream = stream;
+    }
+
+    public void singBeforeQuest(){
+        stream.println("Fa la la, the knight is so brave!");
+    }
+
+    public void singAfterQuest(){
+        stream.println("Tee hee hee, the brave knight " + "did embark on a quest!");
+    }
 }
