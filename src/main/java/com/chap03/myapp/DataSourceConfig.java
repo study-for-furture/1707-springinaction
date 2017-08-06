7831b43697ecb03ed3df8249d1e2c44fd724e4b6
mitmoon2 <mitmoon@gmail.com>
2017-08-06 23:33:57 +0900

    3장 시작

---
+++ src/main/java/com/chap03/myapp/DataSourceConfig.java
@@ -0,0 +1,34 @@
+package com.chap03.myapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by mitmoon on 2017-08-06.
+ */
+@Configuration
+public class DataSourceConfig {
+    @Bean(destroyMethod = "shutdown")
+    @Profile("dev")
+    public DataSource embeddedDataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+    }
+
+    @Bean
+    @Profile("prod")
+    public DataSource jndiDataSource() {
+        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+        jndiObjectFactoryBean.setJndiName("jdbc/myDS");
+        jndiObjectFactoryBean.setResourceRef(true);
+        jndiObjectFactoryBean.setProxyInterface(javax.sql.DataSource.class);
+        return (DataSource) jndiObjectFactoryBean.getObject();
+    }
+}
